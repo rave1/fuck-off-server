1422361c53e1f81c60a1525e5e7aa479566751d4
maciekjasinski <maciek.mj00@gmail.com>
2021-10-08 23:51:31 +0200

    feat: authprovider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { AuthProvider } from './context/auth';
 import { Home } from './pages/JoinChat';
 import { Room } from './pages/Room';
 import { HomePage } from './pages/HomePage';
@@ -9,22 +10,24 @@ import { theme } from './theme';
 function App() {
     return (
         <ThemeProvider theme={theme}>
-            <Router>
-                <Switch>
-                    <Route path="/dashboard/">
-                        <Dashboard />
-                    </Route>
-                    <Route path="/room/:name/">
-                        <Room />
-                    </Route>
-                    <Route path="/join/">
-                        <Home />
-                    </Route>
-                    <Route exact path="/">
-                        <HomePage />
-                    </Route>
-                </Switch>
-            </Router>
+            <AuthProvider>
+                <Router>
+                    <Switch>
+                        <Route path="/dashboard/">
+                            <Dashboard />
+                        </Route>
+                        <Route path="/room/:name/">
+                            <Room />
+                        </Route>
+                        <Route path="/join/">
+                            <Home />
+                        </Route>
+                        <Route exact path="/">
+                            <HomePage />
+                        </Route>
+                    </Switch>
+                </Router>
+            </AuthProvider>
         </ThemeProvider>
     );
 }
