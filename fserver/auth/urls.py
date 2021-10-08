69dc2131e510ec102cc305f16d5ce39faa1e3af5
Jakub Sopel <jakubsopel7@gmail.com>
2021-10-08 20:24:54 +0200

    feat: login and register token based system

---
+++ fserver/auth/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+from auth.views import HelloView, RegisterView
+
+urlpatterns = [
+    path('hello/', HelloView.as_view(), name='hello'),
+    path('login/', obtain_auth_token, name='obtain login'),
+    path('register/', RegisterView.as_view())
+]
---
+++ fserver/auth/views.py
@@ -0,0 +1,23 @@
+from django.template.response import ContentNotRenderedError
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import viewsets
+from rest_framework import status
+from auth.serializers import UserSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
+
+class HelloView(APIView):
+
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        content = {'message': 'HELLO!'}
+        return Response(content)
+    
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny, )
+    http_method_names = ['post']
