0b5740d3232a2a06c787bc06ff01c664b3577746
Jakub Sopel <jakubsopel7@gmail.com>
2021-10-11 19:54:09 +0200

    feat: postgres hookup, work in progress

--- fserver/fserver/settings.py
+++ fserver/fserver/settings.py
@@ -108,8 +108,11 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
