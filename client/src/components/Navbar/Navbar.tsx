21e7ede16713cc4593e41fe69ed38cd4405457ba
daniellisek <lisciu99@gmail.com>
2021-10-11 20:35:57 +0200

    feat: logout function and cursor pointer

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,14 +1,22 @@
+import { useHistory } from "react-router-dom";
 import { PageLogo } from "../PageLogo";
 import { Container, Button } from "./Navbar.styles";
 
 export function Navbar() {
+    const history = useHistory();
+
+    const logout = () => {
+        localStorage.removeItem('token');
+        return history.push('/logout/');
+    };
+    
     return (
         <Container>
             <div>
                 <PageLogo fontSize="36px" />
                 <div>
                     <Button>My profile</Button>
-                    <Button>Logout</Button>
+                    <Button onClick={logout}>Logout</Button>
                 </div>
             </div>
         </Container>
